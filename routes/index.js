3f9cebef4ba334b39922333ea5aa94ca90feb0d1
kandalog <asuasu.tee@gmail.com>
2022-09-23 10:59:22 +0900

    rootを変更

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/api", async (req, res) => {
   return res.status(200).json({ msg: "rootです" });
 });
 
