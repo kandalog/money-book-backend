05ccaa886ec2e3f3e6f82baa86f0d65c3cbdb79c
kandalog <asuasu.tee@gmail.com>
2022-09-23 11:00:19 +0900

    routingを編集

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/api", async (req, res) => {
+router.get("/", async (req, res) => {
   return res.status(200).json({ msg: "rootです" });
+});
+
+router.get("/api", async (req, res) => {
+  return res.status(200).json({ msg: "/apiです" });
 });
 
 module.exports = router;
